59bb30b0b63059a078b85a65c873626ba5165e86
MNASER-HRMIS <m.naser@hrmis.gov.af>
2021-01-04 15:20:31 +0430

    Last commit before end of the day

--- public/js/app.js
+++ public/js/app.js
@@ -21338,8 +21338,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\familytree\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\familytree\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\xampp\htdocs\familytree\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\familytree\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
